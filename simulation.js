59a7b227d57f26c7c9896eba15bc0f8ce2d55207
gravitowl <kjeld.berendsen@gmail.com>
2024-02-03 16:37:41 +0100

    Added sideways groundwater movement.

--- simulation.js
+++ simulation.js
@@ -1,35 +1,79 @@
 let saturationMovePerLoop = 1;
 let evaporationAmount = 0.01;
-let evaporationChance = 0.01;
+let evaporationChance = 0;
 
 function evaporate(x, y) {
-  if (random() < 0.5) modifySaturation(x, y, -evaporationAmount);
+  if (random() < evaporationChance) modifySaturation(x, y, -evaporationAmount);
 }
 
 function moveSaturationDownwards(x, y) {
   let cell = getTile(x, y);
   let cellBeneath = getTile(x, y + 1);
 
+  if (cellBeneath == undefined) return false;
   if (cellBeneath.saturation >= cellBeneath.maxSaturation) return false;
   let maxMovedSaturation = min(saturationMovePerLoop, cell.saturation);
 
   let cellBeneathSpaceLeft = cellBeneath.maxSaturation - cellBeneath.saturation;
-  let maxMovedSaturationBeneath = min(1, cellBeneathSpaceLeft);
-
-  let movedSaturation =
-    min(maxMovedSaturation, maxMovedSaturationBeneath) *
-    cellBeneath.absorbEfficiency;
-
-  modifySaturation(cell.position.x, cell.position.y, -movedSaturation);
-  modifySaturation(
-    cellBeneath.position.x,
-    cellBeneath.position.y,
-    movedSaturation
+  let maxMovedSaturationBeneath = min(
+    saturationMovePerLoop * cellBeneath.absorbEfficiency,
+    cellBeneathSpaceLeft
   );
+
+  let movedSaturation = min(maxMovedSaturation, maxMovedSaturationBeneath);
+
+  modifySaturation(x, y, -movedSaturation);
+  modifySaturation(x, y + 1, movedSaturation);
+
   return true;
 }
 
-function moveSaturationSideways() {}
+function moveSaturationSideways(x, y) {
+  let cell = getTile(x, y);
+  let neighbourOffset = 0;
+
+  if (x == 0) {
+    neighbourOffset = 1;
+  } else if (x == gridSize.w - 1) {
+    neighbourOffset = -1;
+  } else {
+    neighbourOffset = random([-1, 1]);
+  }
+
+  let neighbourCell = getTile(x + neighbourOffset, y);
+
+  if (neighbourCell == undefined) return false;
+  if (neighbourCell.saturation >= neighbourCell.maxSaturation) return false;
+  if (neighbourCell.saturation >= cell.saturation) return false;
+
+  let saturationDifference =
+    abs(cell.saturation - neighbourCell.saturation) / 2;
+
+  if (saturationDifference < 0.1) return false;
+
+  let maxMovedSaturation = min(
+    min(saturationDifference, saturationMovePerLoop),
+    cell.saturation
+  );
+
+  let neighbourSpaceLeft =
+    neighbourCell.maxSaturation - neighbourCell.saturation;
+
+  let maxNeighbourMovedSaturation = min(
+    min(
+      saturationDifference,
+      saturationMovePerLoop * neighbourCell.absorbEfficiency
+    ),
+    neighbourSpaceLeft
+  );
+
+  let movedSaturation = min(maxMovedSaturation, maxNeighbourMovedSaturation);
+
+  modifySaturation(x, y, -movedSaturation);
+  modifySaturation(x + neighbourOffset, y, movedSaturation);
+
+  return true;
+}
 
 function simulateGroundWater() {
   for (let i = 0; i < gridSize.w; i++) {
@@ -38,9 +82,10 @@ function simulateGroundWater() {
 
       evaporate(i, j);
 
-      if (getTile(i, j + 1) == undefined) continue;
-
       let moveDownSuccess = moveSaturationDownwards(i, j);
+      if (moveDownSuccess) continue;
+
+      moveSaturationSideways(i, j);
     }
   }
 }
